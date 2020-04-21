b08de41512f3bb87896d46696afeb479142345d9
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2020-04-20 22:46:28 -0300

    Added new file to represent actions of Worker model

---
+++ app/Http/Controllers/WorkerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WorkerController extends Controller
+{
+    public function displayHourlyWorks() {
+        return 'some values';
+    }
+}
